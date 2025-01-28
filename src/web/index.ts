40d56c7ebb2468de47e7bb0b4d3eb54b9d60ab54
Andrew Mackrodt <andrewmackrodt@gmail.com>
2025-01-28 21:36:54 +0000

    fix: serve correct approot path for web server when running with ts-node

--- src/web/index.ts
+++ src/web/index.ts
@@ -1,6 +1,6 @@
 import {IncomingMessage, Server, ServerResponse, createServer} from 'http';
 import {config, setConfig} from '../config';
-import {createReadStream, readdir} from 'fs';
+import {createReadStream, existsSync, readdir} from 'fs';
 import {
   getAllBrands,
   getAllModels,
@@ -11,7 +11,14 @@ import {
 import {isAbsolute, join, normalize, relative} from 'path';
 import {logger} from '../logger';
 
-const approot = join(__dirname, '../../../');
+const approot = (function () {
+  const path = join(__dirname, '../..');
+  if (existsSync(join(path, 'web'))) {
+    return path;
+  } else {
+    return join(path, '..');
+  }
+})();
 const webroot = join(approot, './web');
 const screenshotDir = join(approot, config.page.screenshotDir);
 
